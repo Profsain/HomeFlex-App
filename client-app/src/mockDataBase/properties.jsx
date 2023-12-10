6520ef629bac6ad7054d049991a5a55e4a56b606
olanrewaju adebayo <adebayoolanrewaju970@gamail.com>
2023-12-10 04:54:34 +0100

    #3 added the needed mock data for the properties

---
+++ client-app/src/mockDataBase/properties.jsx
@@ -0,0 +1,156 @@
+// mockData.js
+const properties = [
+    {
+      propertyImage: "/mockDataImagies/image1.jpg",
+      propertyAddress: "15 Adeola Odeku Street",
+      propertyType: "Apartment",
+      propertyLocation: "Victoria Island, Lagos",
+    },
+    {
+      propertyImage: "/mockDataImagies/image2.jpg",
+      propertyAddress: "27 Obafemi Awolowo Way",
+      propertyType: "House",
+      propertyLocation: "Ikeja, Lagos",
+    },
+    {
+      propertyImage: "/mockDataImagies/image3.jpg",
+      propertyAddress: "8 Azikiwe Street",
+      propertyType: "Condo",
+      propertyLocation: "Port Harcourt, Rivers",
+    },
+    {
+      propertyImage: "/mockDataImagies/image4.jpg",
+      propertyAddress: "21 Emeka Anyaoku Street",
+      propertyType: "Duplex",
+      propertyLocation: "Enugu",
+    },
+    {
+      propertyImage: "/mockDataImagies/image5.jpg",
+      propertyAddress: "14 Oba Adebimpe Road",
+      propertyType: "Townhouse",
+      propertyLocation: "Ilorin, Kwara",
+    },
+    {
+      propertyImage: "/mockDataImagies/image6.jpg",
+      propertyAddress: "5 Shehu Shagari Way",
+      propertyType: "Apartment",
+      propertyLocation: "Abuja",
+    },
+    {
+      propertyImage: "/mockDataImagies/image7.jpg",
+      propertyAddress: "9 Ahmadu Bello Avenue",
+      propertyType: "House",
+      propertyLocation: "Kano",
+    },
+    {
+      propertyImage: "/mockDataImagies/image8.jpg",
+      propertyAddress: "33 Aguiyi Ironsi Street",
+      propertyType: "Condo",
+      propertyLocation: "Abakaliki, Ebonyi",
+    },
+    {
+      propertyImage: "/mockDataImagies/image9.jpg",
+      propertyAddress: "18 Herbert Macaulay Way",
+      propertyType: "Duplex",
+      propertyLocation: "Abeokuta, Ogun",
+    },
+    {
+      propertyImage: "/mockDataImagies/image10.jpg",
+      propertyAddress: "22 Yakubu Gowon Crescent",
+      propertyType: "Apartment",
+      propertyLocation: "Jos, Plateau",
+    },
+    {
+      propertyImage: "/mockDataImagies/image11.jpg",
+      propertyAddress: "7 Goodluck Jonathan Drive",
+      propertyType: "Townhouse",
+      propertyLocation: "Yenagoa, Bayelsa",
+    },
+    {
+      propertyImage: "/mockDataImagies/image12.jpg",
+      propertyAddress: "12 Nnamdi Azikiwe Avenue",
+      propertyType: "House",
+      propertyLocation: "Owerri, Imo",
+    },
+    {
+      propertyImage: "/mockDataImagies/image13.jpg",
+      propertyAddress: "6 Tafawa Balewa Way",
+      propertyType: "Condo",
+      propertyLocation: "Bauchi",
+    },
+    {
+      propertyImage: "/mockDataImagies/image14.jpg",
+      propertyAddress: "10 Anthony Enahoro Street",
+      propertyType: "Duplex",
+      propertyLocation: "Ado-Ekiti, Ekiti",
+    },
+    {
+      propertyImage: "/mockDataImagies/image15.jpg",
+      propertyAddress: "17 Ibrahim Babangida Crescent",
+      propertyType: "Apartment",
+      propertyLocation: "Minna, Niger",
+    },
+    {
+      propertyImage: "/mockDataImagies/image16.jpg",
+      propertyAddress: "29 Yakubu Bako Street",
+      propertyType: "Townhouse",
+      propertyLocation: "Katsina",
+    },
+    {
+      propertyImage: "/mockDataImagies/image17.jpg",
+      propertyAddress: "3 Funmilayo Ransome-Kuti Street",
+      propertyType: "House",
+      propertyLocation: "Abeokuta, Ogun",
+    },
+    {
+      propertyImage: "/mockDataImagies/image18.jpg",
+      propertyAddress: "42 IBB Way",
+      propertyType: "Duplex",
+      propertyLocation: "Abuja",
+    },
+    {
+      propertyImage: "/mockDataImagies/image19.jpg",
+      propertyAddress: "14 Awolowo Road",
+      propertyType: "Townhouse",
+      propertyLocation: "Ikoyi, Lagos",
+    },
+    {
+      propertyImage: "/mockDataImagies/image20.jpg",
+      propertyAddress: "5 Murtala Mohammed Drive",
+      propertyType: "Apartment",
+      propertyLocation: "Calabar, Cross River",
+    },
+    {
+      propertyImage: "/mockDataImagies/image21.jpg",
+      propertyAddress: "23 Funsho Williams Avenue",
+      propertyType: "House",
+      propertyLocation: "Surulere, Lagos",
+    },
+    {
+      propertyImage: "/mockDataImagies/image22.jpg",
+      propertyAddress: "16 Okpara Avenue",
+      propertyType: "Condo",
+      propertyLocation: "Enugu",
+    },
+    {
+      propertyImage: "/mockDataImagies/image23.jpg",
+      propertyAddress: "31 Adekunle Fajuyi Way",
+      propertyType: "Duplex",
+      propertyLocation: "Ibadan, Oyo",
+    },
+    {
+      propertyImage: "/mockDataImagies/image24.jpg",
+      propertyAddress: "9 Ahmadu Bello Crescent",
+      propertyType: "Townhouse",
+      propertyLocation: "Kaduna",
+    },
+    {
+      propertyImage: "/mockDataImagies/image25.jpg",
+      propertyAddress: "4 Queen Elizabeth II Crescent",
+      propertyType: "Apartment",
+      propertyLocation: "Asaba, Delta",
+    },
+  ];
+  
+  export default properties;
+  
