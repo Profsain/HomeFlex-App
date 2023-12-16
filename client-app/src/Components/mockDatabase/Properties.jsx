f413d4b9799f15ddb106f48a7c5f7f5b7c52e56b
profsain <husseinimudiking@gmail.com>
2023-12-16 16:47:55 +0100

    remove public path

--- client-app/src/Components/mockDatabase/Properties.jsx
+++ client-app/src/Components/mockDatabase/Properties.jsx
@@ -1,175 +1,175 @@
 // mockData.js
 const properties = [
     {
-      propertyImage: "public/mockDataImagies/image1.jpg",
+      propertyImage: "mockDataImagies/image1.jpg",
       propertyAddress: "15 Adeola Odeku Street",
       propertyType: "Apartment",
       propertyLocation: "Victoria Island, Lagos",
       price: "5.9 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image2.jpg",
+      propertyImage: "mockDataImagies/image2.jpg",
       propertyAddress: "27 Obafemi Awolowo Way",
       propertyType: "House",
       propertyLocation: "Ikeja, Lagos",
       price: "5 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image3.jpg",
+      propertyImage: "mockDataImagies/image3.jpg",
       propertyAddress: "8 Azikiwe Street",
       propertyType: "Condo",
       propertyLocation: "Port Harcourt, Rivers",
       price: "8 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image4.jpg",
+      propertyImage: "mockDataImagies/image4.jpg",
       propertyAddress: "21 Emeka Anyaoku Street",
       propertyType: "Duplex",
       propertyLocation: "Enugu",
       price: "12 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image5.jpg",
+      propertyImage: "mockDataImagies/image5.jpg",
       propertyAddress: "14 Oba Adebimpe Road",
       propertyType: "Townhouse",
       propertyLocation: "Ilorin, Kwara",
       price: "4.6 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image6.jpg",
+      propertyImage: "mockDataImagies/image6.jpg",
       propertyAddress: "5 Shehu Shagari Way",
       propertyType: "Apartment",
       propertyLocation: "Abuja",
       price: "34.1 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image7.jpg",
+      propertyImage: "mockDataImagies/image7.jpg",
       propertyAddress: "9 Ahmadu Bello Avenue",
       propertyType: "House",
       propertyLocation: "Kano",
       price: "7.4 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image8.jpg",
+      propertyImage: "mockDataImagies/image8.jpg",
       propertyAddress: "33 Aguiyi Ironsi Street",
       propertyType: "Condo",
       propertyLocation: "Abakaliki, Ebonyi",
       price: "5 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image9.jpg",
+      propertyImage: "mockDataImagies/image9.jpg",
       propertyAddress: "18 Herbert Macaulay Way",
       propertyType: "Duplex",
       propertyLocation: "Abeokuta, Ogun",
       price: "15.3 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image10.jpg",
+      propertyImage: "mockDataImagies/image10.jpg",
       propertyAddress: "22 Yakubu Gowon Crescent",
       propertyType: "Apartment",
       propertyLocation: "Jos, Plateau",
       price: "19 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image11.jpg",
+      propertyImage: "mockDataImagies/image11.jpg",
       propertyAddress: "7 Goodluck Jonathan Drive",
       propertyType: "Townhouse",
       propertyLocation: "Yenagoa, Bayelsa",
       price: "29 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image12.jpg",
+      propertyImage: "mockDataImagies/image12.jpg",
       propertyAddress: "12 Nnamdi Azikiwe Avenue",
       propertyType: "House",
       propertyLocation: "Owerri, Imo",
       price: "15 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image13.jpg",
+      propertyImage: "mockDataImagies/image13.jpg",
       propertyAddress: "6 Tafawa Balewa Way",
       propertyType: "Condo",
       propertyLocation: "Bauchi",
       price: "13 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image14.jpg",
+      propertyImage: "mockDataImagies/image14.jpg",
       propertyAddress: "10 Anthony Enahoro Street",
       propertyType: "Duplex",
       propertyLocation: "Ado-Ekiti, Ekiti",
       price: "10 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image15.jpg",
+      propertyImage: "mockDataImagies/image15.jpg",
       propertyAddress: "17 Ibrahim Babangida Crescent",
       propertyType: "Apartment",
       propertyLocation: "Minna, Niger",
       price: "5 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image16.jpg",
+      propertyImage: "mockDataImagies/image16.jpg",
       propertyAddress: "29 Yakubu Bako Street",
       propertyType: "Townhouse",
       propertyLocation: "Katsina",
       price: "9 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image17.jpg",
+      propertyImage: "mockDataImagies/image17.jpg",
       propertyAddress: "3 Funmilayo Ransome-Kuti Street",
       propertyType: "House",
       propertyLocation: "Abeokuta, Ogun",
       price: "12 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image18.jpg",
+      propertyImage: "mockDataImagies/image18.jpg",
       propertyAddress: "42 IBB Way",
       propertyType: "Duplex",
       propertyLocation: "Abuja",
       price: "50 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image19.jpg",
+      propertyImage: "mockDataImagies/image19.jpg",
       propertyAddress: "14 Awolowo Road",
       propertyType: "Townhouse",
       propertyLocation: "Ikoyi, Lagos",
       price: "129 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image20.jpg",
+      propertyImage: "mockDataImagies/image20.jpg",
       propertyAddress: "5 Murtala Mohammed Drive",
       propertyType: "Apartment",
       propertyLocation: "Calabar, Cross River",
       price: "5.9 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image21.jpg",
+      propertyImage: "mockDataImagies/image21.jpg",
       propertyAddress: "23 Funsho Williams Avenue",
       propertyType: "House",
       propertyLocation: "Surulere, Lagos",
       price: "8 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image22.jpg",
+      propertyImage: "mockDataImagies/image22.jpg",
       propertyAddress: "16 Okpara Avenue",
       propertyType: "Condo",
       propertyLocation: "Enugu",
       price: "5 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image23.jpg",
+      propertyImage: "mockDataImagies/image23.jpg",
       propertyAddress: "31 Adekunle Fajuyi Way",
       propertyType: "Duplex",
       propertyLocation: "Ibadan, Oyo",
       price: "14 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image24.jpg",
+      propertyImage: "mockDataImagies/image24.jpg",
       propertyAddress: "9 Ahmadu Bello Crescent",
       propertyType: "Townhouse",
       propertyLocation: "Kaduna",
       price: "2.9 million"
     },
     {
-      propertyImage: "public/mockDataImagies/image25.jpg",
+      propertyImage: "mockDataImagies/image25.jpg",
       propertyAddress: "4 Queen Elizabeth II Crescent",
       propertyType: "Apartment",
       propertyLocation: "Asaba, Delta",
